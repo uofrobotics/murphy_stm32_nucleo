492935e0a020c1e41f27aa9fb0344c124f4b669c
Karim Ait-Allaoua <kaitallaoua@gmail.com>
2023-12-05 14:33:34 -0600

    motor driver state machine works

--- code/jetson_comms.c
+++ code/jetson_comms.c
@@ -43,50 +43,109 @@ void setup_jetson_serial(void) {
 
 void vJetson_packet_parser (void* params) {
 	uint8_t packet;
+ struct drive_motor_state drive_mt_state = {0};
+
+	
 	TickType_t xLastWakeTime;
-	const TickType_t xPeriod = pdMS_TO_TICKS( 50 );
+	const TickType_t xPeriod = pdMS_TO_TICKS( 10 );
 	xLastWakeTime = xTaskGetTickCount();
 	
     while (1) {
         if (xQueueReceive(jetson_packet_char_q, &packet, portMAX_DELAY) == pdPASS) {
-						send_jetson_byte(packet, 10);
+// data packet
+				if (packet <= 127) {
 					
-						switch(packet) {
-							case 'f':
-								unlock_all_motors_moveable();
-								control_sabertooth(SABER_ADDR_FRONT, SABER_CMD_FWD_M1, 30);
-								control_sabertooth(SABER_ADDR_FRONT, SABER_CMD_FWD_M2, 30);
-								
-								control_sabertooth(SABER_ADDR_BACK, SABER_CMD_FWD_M1, 30);
-								control_sabertooth(SABER_ADDR_BACK, SABER_CMD_FWD_M2, 30);
-							control_green_indicator(1);
-								control_red_indicator(0);
-							control_amber_indicator(0);
-								break;
-							case 'r':
-								control_sabertooth(SABER_ADDR_FRONT, SABER_CMD_REV_M1, 30);
-								control_sabertooth(SABER_ADDR_FRONT, SABER_CMD_REV_M2, 30);
-								
-								control_sabertooth(SABER_ADDR_BACK, SABER_CMD_REV_M1, 30);
-								control_sabertooth(SABER_ADDR_BACK, SABER_CMD_REV_M2, 30);
-							control_green_indicator(1);
-															control_red_indicator(0);
-														control_amber_indicator(0);
-
-
-								break;
-							case 's':
-								//stop_drive_motors();
-								lock_all_motors_stopped();
-								control_red_indicator(1);
-							control_green_indicator(0);
+					
+					// are we expecting it?
+					if (drive_mt_state.packet_count >=1) {
+						
+						// keeping track of packet count is how we know the order of packets
+						switch (drive_mt_state.packet_count) {
 							
+							// BL,128,m1
+							case 1:
+								control_sabertooth(SABER_ADDR_BACK, drive_mt_state.m1_128_com, packet);
 								break;
 							
+							// BR,128,m2
+							case 2:
+								control_sabertooth(SABER_ADDR_BACK, drive_mt_state.m2_128_com, packet);
+								break;
+							
+							// FL,129,m1
+							case 3:
+								control_sabertooth(SABER_ADDR_FRONT, drive_mt_state.m1_129_com, packet);
+								break;
+							
+							// FR,129,m2
+							case 4:
+								control_sabertooth(SABER_ADDR_FRONT, drive_mt_state.m2_129_com, packet);
+								break;
+							
+							// not sure what a default case looks like, probably error condition
 						}
+						
+						
+						
+						// if finished with all packets, set packet count to zero
+						if (drive_mt_state.packet_count == 4) {
+							drive_mt_state.packet_count = 0;
+							
+						} 
+						
+						// else normally increment
+						else {
+							drive_mt_state.packet_count++;
+						}
+						
+					
+					}
+					
+					
+					// other things could be using this data, check here
+					
 					
 				}
+				
+				/*
+				// heartbeat, respond immediately
+				else if (packet == 128) {
 					
+					// really shouldn't be waiting much at all
+					xQueueSendToBack(heartbeat_q, &heartbeat_state, pdMS_TO_TICKS(2));
+					send_jetson_byte(128, 10);
+				}
+				*/
+				// drive motor control
+				 else if(packet >= 129 && packet <= 145) {
+					 
+					 // each bit represents the direction for each motor
+					 /*
+					 BL,128,m1   BR,128,m2   FL,129,m1  FR,129,m2
+			bit  3                2                  1                0
+					 
+					 where 0 is forward, 1 is reverse
+					 */
+					 uint8_t drive_motor_dir = packet - DRIVE_MOTOR_CTRL_START;
+					 
+					 drive_mt_state.m1_128_com = (drive_motor_dir & M1_128_MASK) >> 3;
+					 
+					 // 4 -> drive forward motor 2
+					 // 5 -> drive backwards motor 2
+					 drive_mt_state.m2_128_com = ( (drive_motor_dir & M2_128_MASK) >> 2) + 4;
+					 
+					 drive_mt_state.m1_129_com = (drive_motor_dir & M1_129_MASK) >> 1;
+					 
+					 drive_mt_state.m2_129_com = (drive_motor_dir & M2_129_MASK) + 4;
+					 
+					 // do not increment, keep count at one if they send many drive control packets
+					 drive_mt_state.packet_count = 1;
+					 
+				 }
+					
+					
+					
+				}
 				vTaskDelayUntil( &xLastWakeTime, xPeriod );
     }
 	
--- code/jetson_comms.h
+++ code/jetson_comms.h
@@ -8,3 +8,11 @@ int send_jetson_byte(uint8_t byte, uint32_t retries);
 
 void USART3_IRQHandler(void);
 
+struct drive_motor_state {
+	uint8_t m1_128_com;
+	uint8_t m2_128_com;
+	uint8_t m1_129_com;
+	uint8_t m2_129_com;
+	uint8_t packet_count;
+	
+};
